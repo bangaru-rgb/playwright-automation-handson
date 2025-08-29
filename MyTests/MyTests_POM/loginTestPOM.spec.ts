80dd468b4ab771f9e0d6766e6481bf4c9be0af51
Bangaru <bangarunaidu@gmail.com>
2025-08-29 10:18:07 +0530

    Fxing POM file

--- MyTests/MyTests_POM/loginTestPOM.spec.ts
+++ MyTests/MyTests_POM/loginTestPOM.spec.ts
@@ -15,20 +15,3 @@ test('login using POM', async ({ page }) => {
   await HP_LTP.HomepageValidation();
 
 });
-// loginTest.spec.ts
-import { test, expect, chromium, Page } from '@playwright/test';
-import { LoginPage } from '../../Pages/LoginPage';
-//import { homePage } from '../../Pages/homePage';
-
-test('login using POM', async ({ page }) => {
-
-  const loginPage = new LoginPage(page);
-  console.log("Test started at:", new Date().toISOString());
-  //console.log("You are calling Login function to enter credentails.");
-  await loginPage.launchTheURL();
-
-
-  const HP_LTP = await loginPage.login('bangarunaidu@gmail.com', 'Test@123');
-  await HP_LTP.HomepageValidation();
-
-});
