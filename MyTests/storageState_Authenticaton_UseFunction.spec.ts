d80795cb4a405a343b2591aa199545954b0caa42
Bangaru <bangarunaidu@gmail.com>
2025-08-29 10:43:43 +0530

    storageState_Authenticaton_UseFunction

--- MyTests/storageState_Authenticaton_UseFunction.spec.ts
+++ MyTests/storageState_Authenticaton_UseFunction.spec.ts
@@ -5,8 +5,8 @@ import { register } from "module";
 import { chromium, webkit, firefox } from "playwright";
 const authfile = "Testdata/auth.json";
 
-//use: {storageState: authfile};
-test('login test', async () => {
+//use: {storageState: authfile}; this can be used in playwright.config.ts
+test.skip('login test', async () => {
   const browser: Browser = await chromium.launch({ headless: false, channel: 'chrome' });
 
   const page: Page = await browser.newPage()
@@ -29,11 +29,20 @@ test('login test', async () => {
 
 });
 test.use({ storageState: authfile });
+// the above statement means, all the tests in this page can use this authfile. if you want only the below test has too use this auth file  then you have  to use the below code.
+// test.describe('Authenticated tests', () => {
+//   test.use({ storageState: authfile });
+
+//   test('MyAccount test', async ({ page }) => {
+//     // logged in
+//   });
+// });
+
 test('MyAccount test', async ({ page }) => {
   //const browser: Browser = await chromium.launch({ headless: false, channel: 'chrome' });
   // const browsweContext1:BrowserContext = await browser.newContext({storageState: authfile});
   // const page: Page = await browsweContext1.newPage()
-
+  console.log ("We are in my account test ok");
   await page.goto("https://naveenautomationlabs.com/opencart/index.php?route=account/account");
   await page.getByRole('link', { name: 'Edit your account information' }).click();
   await page.screenshot({ path: 'Account Info.png' });
